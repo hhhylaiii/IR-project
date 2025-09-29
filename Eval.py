62b000a41bdedbece6e8670f6ff61d505b56a34e
hhhylaiii <owen131855773@gmail.com>
2025-09-29 18:58:58 +0800

    Complete all requirements of Task 4

--- Eval.py
+++ Eval.py
@@ -1,2 +1,59 @@
-from Parser import Parser
-from main import VectorSpace
+import os
+from VectorSpace import VectorSpace
+import numpy as np
+
+def load_smaller_dataset(root="smaller_dataset"):
+    vs = VectorSpace()
+    doc_names, documents = vs.load_documents(os.path.join(root, "collections"))
+    quer_names, queries = vs.load_documents(os.path.join(root, "queries"))
+
+    relevant={}
+    with open(os.path.join(root, "rel.tsv"), 'r', encoding='utf-8') as f:
+        for line in f:
+            qid, did_list_srt = line.strip().split('\t')
+            did_list = did_list_srt.strip("[]").split(',')
+            relevant[qid] = set([int(p.strip()) for p in did_list if p.strip()])
+    return doc_names, documents, quer_names, queries, relevant
+
+def build_index(documents, doc_names):
+    vs = VectorSpace()
+    vs.doc_names = doc_names
+    vs.build(documents)
+    return vs
+
+def single_query_topk(query, index:VectorSpace, k=10, use_tfidf=True):
+    topk, _, _ =index.search([query], use_tfidf=use_tfidf)
+    return [int(name[1:-4]) for name, _ in topk[:k]]
+
+def rr_at_10(pred_ids, relevant_set):
+    for rank, pid in enumerate(pred_ids[:10], 1):
+        if pid in relevant_set:
+            return 1.0 / rank
+    return 0.0
+
+def ap_at_10(pred_ids, relevant_set):
+    hits = 0
+    ap = 0.0
+    for rank, pid in enumerate(pred_ids[:10], 1):
+        if pid in relevant_set:
+            hits += 1
+            ap += hits / rank
+    rel_nums = min(len(relevant_set), 10)
+    return ap / rel_nums if rel_nums > 0 else 0.0
+
+def recall_at_10(pred_ids, relevant_set):
+    return len(set(pred_ids[:10]) & relevant_set) / len(relevant_set) if len(relevant_set) > 0 else 0.0
+
+def evaluate_all(doc_names, documents, quer_names, queries, relevant, k=10, use_tfidf=True):
+    index = build_index(documents, doc_names)
+    rr_list, ap_list, rc_list = [], [], []
+    for qname, query in zip(quer_names, queries):
+        qid = os.path.splitext(qname)[0]
+        relevant_set = relevant.get(qid, set())
+        
+        pred_ids = single_query_topk(query, index, k=k, use_tfidf=use_tfidf)
+        rr_list.append(rr_at_10(pred_ids, relevant_set))
+        ap_list.append(ap_at_10(pred_ids, relevant_set))
+        rc_list.append(recall_at_10(pred_ids, relevant_set))
+
+    return float(np.mean(rr_list)), float(np.mean(ap_list)), float(np.mean(rc_list))
--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import argparse
 from VectorSpace import VectorSpace
 from ParserZH import ParserZH
+from Eval import load_smaller_dataset, evaluate_all
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Information Retrieval Assignment")
     parser.add_argument("--en_query", type=str, help="English query string")
     parser.add_argument("--feedback", action="store_true", help="Enable pseudo relevance feedback")
     parser.add_argument("--ch_query", type=str, help="Chinese query string")
+    parser.add_argument("--eval", action="store_true", help="Run Task 4 evaluation")
     args = parser.parse_args()
 
-    if not args.en_query and not args.ch_query:
-        print("Error: Please provide either --en_query <text> or --ch_query <文字>.")
-        exit(1)
+    if not (args.en_query or args.ch_query or args.eval or args.feedback):
+        parser.error("Please provide at least one of --en_query, --ch_query, or --eval.")
+
+    if args.feedback and not args.en_query:
+        parser.error("--feedback requires --en_query.")
 
     if args.en_query:
         vectorSpace = VectorSpace()
@@ -20,7 +24,7 @@ if __name__ == '__main__':
 
         resultswithcosineTF, resultswitheuclidean_s_TF, resultswitheuclidean_d_TF = vectorSpace.search(args.en_query.split(), use_tfidf=False)
         resultswithcosineTFIDF, resultswitheuclidean_s_TFIDF, resultswitheuclidean_d_TFIDF = vectorSpace.search(args.en_query.split(), use_tfidf=True)
-
+        
         if not args.feedback:
             print("English TF Cosine")
             print(f"{'NewsID':<15}{'Score':>6}")
@@ -74,3 +78,13 @@ if __name__ == '__main__':
         for name, score in results_TFIDF[:10]:
             print(f"{name:<15}{score:>10.7f}")
         print("-"*40)
+    
+    if args.eval:
+        doc_names, documents, quer_names, queries, relevant = load_smaller_dataset()
+        mrr, map10, r10 = evaluate_all(doc_names, documents, quer_names, queries, relevant, 10, True)
+
+        print("Evaluation IR system with TF-IDF Cosine")
+        print(f"MRR@10         {mrr:.6f}")
+        print(f"MAP@10         {map10:.6f}")
+        print(f"RECALL@10      {r10:.6f}")
+        print("-"*24)
